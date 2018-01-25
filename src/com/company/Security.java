a2ebdd56cdf13b2dec3c3870dd9c7c19c1ca0f0e
TarasVolianskyi <volianskyitv@gmail.com>
2018-01-25 23:05:36 +0200

    refactored class Security add new object password

--- src/com/company/Security.java
+++ src/com/company/Security.java
@@ -1,4 +1,25 @@
 package com.company;
 
 public class Security {
+
+    private String password;
+
+    public Security(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Security{" +
+                "password='" + password + '\'' +
+                '}';
+    }
 }
